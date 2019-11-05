1b4d3ccfea6e4c025d79691c982c218a4bb30aee
Noah GO Developer <developer.chain.2019@hotmail.com>
2019-11-05 19:21:32 +0400

    service for daily auto-rewards

---
+++ cmd/service/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/noah-blockchain/Auto-rewards/app"
+	"github.com/noah-blockchain/Auto-rewards/config"
+	"github.com/noah-blockchain/go-sdk/wallet"
+	"github.com/robfig/cron/v3"
+)
+
+var (
+	cfg           = config.Config{}
+	cronScheduler = cron.New(
+		cron.WithParser(
+			cron.NewParser(
+				cron.Descriptor|cron.SecondOptional|cron.Minute|cron.Hour|cron.Dom|cron.Month|cron.Dow,
+			),
+		),
+		cron.WithLogger(
+			cron.VerbosePrintfLogger(
+				log.New(os.Stdout,
+					"go cron scheduler: ",
+					log.LstdFlags|log.Lmicroseconds|log.Llongfile|log.Lshortfile,
+				),
+			),
+		),
+		cron.WithLocation(time.FixedZone("Europe/Moscow", 0)),
+	)
+)
+
+func init() {
+	minCoinDelegated := config.FloatGetEnv(os.Getenv("MIN_COINS_DELEGATED"), 100)
+	cfg.StopListAccounts = strings.Split(os.Getenv("STOP_LIST"), ",")
+	flag.StringVar(&cfg.SeedPhrase, "seed.phrase", os.Getenv("SEED_PHRASE"), "seed phrase not exist")
+	flag.StringVar(&cfg.BaseCoin, "base.coin", os.Getenv("BASE_COIN"), "base coin not exist")
+	flag.StringVar(&cfg.NodeApiURL, "node.api_url", os.Getenv("NODE_API_URL"), "node api url not exist")
+	flag.StringVar(&cfg.ExplorerApiURL, "explorer.api_url", os.Getenv("EXPLORER_API_URL"), "explorer api url not exist")
+	flag.StringVar(&cfg.Token, "token", os.Getenv("TOKEN"), "token not exist")
+	flag.Float64Var(&cfg.MinCoinDelegated, "min_coin_delegated", minCoinDelegated, "min coin delegated not setup")
+}
+
+func main() {
+	flag.Usage = func() {
+		cmd := strings.TrimSuffix(path.Base(os.Args[0]), ".test")
+		fmt.Printf("Usage of %s:\n", cmd)
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	switch {
+	case cfg.SeedPhrase == "":
+		log.Panicf("Invalid value %s for field %s", cfg.SeedPhrase, "seed.phrase")
+	case cfg.BaseCoin == "":
+		log.Panicf("Invalid value %s for field %s", cfg.BaseCoin, "base.coin")
+	case cfg.NodeApiURL == "":
+		log.Panicf("Invalid value %s for field %s", cfg.NodeApiURL, "node.api_url")
+	case cfg.ExplorerApiURL == "":
+		log.Panicf("Invalid value %s for field %s", cfg.ExplorerApiURL, "explorer.api_url")
+	case cfg.Token == "":
+		log.Panicf("Invalid value %s for field %s", cfg.Token, "token")
+	}
+
+	_, _ = cronScheduler.AddFunc("00 14 * * * *", func() {
+
+		seed, _ := wallet.Seed(cfg.SeedPhrase)
+		walletFrom, err := wallet.NewWallet(seed)
+		if err != nil {
+			log.Panicln(err)
+		}
+		log.Println("Wallet was successful received.")
+
+		autoRewards := app.NewAutoRewards(cfg)
+		multiSend, err := autoRewards.CreateMultiSendList(walletFrom.Address(), cfg.BaseCoin)
+		if err != nil || multiSend == nil {
+			log.Panicln(err)
+		}
+		log.Println("Multi list for accounts was successful created.")
+
+		if err = autoRewards.SendMultiAccounts(walletFrom, *multiSend, "Payment from app", cfg.BaseCoin); err != nil {
+			log.Panicln(err)
+		}
+		log.Println("All multi accounts was successful transferred.")
+
+	})
+	cronScheduler.Start()
+
+	select {}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/noah-blockchain/Auto-rewards
 
 go 1.12
 
-require github.com/noah-blockchain/go-sdk v0.7.1
+require (
+	github.com/noah-blockchain/go-sdk v0.7.1
+	github.com/robfig/cron/v3 v3.0.0
+)
